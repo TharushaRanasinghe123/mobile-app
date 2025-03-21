d9791805feaffbc3c36d811a040d31572c02fba6
R.M.T.Pradeepani <pradeepani_rmt_e23@engug.ruh.ac.lk>
2025-03-21 21:26:23 +0530

    update

--- project/inc/dbcon.php
+++ project/inc/dbcon.php
@@ -1,5 +1,9 @@
 <?php
-$connection = mysqli_connect("localhost", "root", "123456", "ImageDB");
+// Include config file
+require_once 'config.php';
+
+// Connect using configuration constants
+$connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 if (!$connection) {
     die("Database connection failed: " . mysqli_connect_error());
